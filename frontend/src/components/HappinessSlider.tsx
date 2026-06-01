8c7d3e662a127d1415b121f3edfb9d376a4a5cef
Maia Grisch <thisisanimosity@hotmail.com>
2026-06-01 10:38:54 -0700

    Fixing slider to work as slider not discrete values

--- frontend/src/components/HappinessSlider.tsx
+++ frontend/src/components/HappinessSlider.tsx
@@ -5,8 +5,7 @@ import {
 } from "@bcgov/design-system-react-components";
 
 import React, {useState} from 'react';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Slider from '@mui/material';
 
 
 interface HappinessSliderProps {
@@ -19,9 +18,9 @@ function HappinessSlider({
     onSelect
 }: HappinessSliderProps) {
 
-    if (!isOpen) return null;
-
     const [value, setValue]= useState(3);
+
+    if (!isOpen) return null;
 
     const emojis = [
         "😡",
@@ -31,16 +30,35 @@ function HappinessSlider({
         "😁"
     ];
 
-    const currentEmoji = emojis[value-1];
-
     const handleSliderChange = (newValue: number) => {
         setValue(newValue);
         console.log(newValue);
     }
 
     const EmojiHandle = (props:any) => {
-        const {value, dragging, index, ...restProps } = props;
-    
+        const handleValue = props.value;
+        
+        return (
+            <div 
+                {...props}
+                style={{
+                    width: "40px",
+                    height: "40px",
+                    fontSize: "2rem",
+                    background: "transparent",
+                    border: "none",
+                    cursor: "grab",
+                    textAlign: "center",
+                    lineHeight: "40px",
+                    marginTop: "-10px"
+                }}
+            >
+                {emojis[handleValue - 1]}
+            </div>
+        );
+    };
+
+
         return (
             <Modal isOpen={isOpen}>
                 <Dialog aria-label="Happiness rating">
@@ -54,14 +72,43 @@ function HappinessSlider({
                                 justifyContent: "center"
                             }}
                         >
-                            <Slider value={value} onChange={handleSliderChange}/>
-                        
+                            {emoji[value -1]}
+                            </div>
+                            <Slider 
+                                min={1}
+                                max={5}
+                                step={1}
+                                value={value} 
+
+                                onChange={(newValue) => 
+                                    handleSliderChange(newValue as number)
+                                }
+                                onChangeComplete={(newValue) => 
+                                    onSelect(newValue as number)
+                                }
+                                handleRender={(node, props) => (
+                                    <EmojiHandle {...props} />
+                                )}
+                            />
+
+                            <div>
+                                style=={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    marginTop: "1rem",
+                                    fontSize: "2rem"
+                                }}
+                                
+                                {emojis.map((emoji) => (
+                                    <span key={emoji}>{emoji}</span>
+                                ))}
+                            
                         </div>
                     </div>
                 </Dialog>
             </Modal>
         );
     }
-}
+
 
 export default HappinessSlider;
