cadf39cd908443e6dcceec41bff44e8e38c376a0
Maia Grisch <thisisanimosity@hotmail.com>
2026-06-01 20:40:53 -0700

    Corrected HappinessSlider into component to be rendered by feedback form

--- frontend/src/components/HappinessSlider.tsx
+++ frontend/src/components/HappinessSlider.tsx
@@ -1,114 +1,43 @@
-import {
-    Button,
-    Dialog,
-    Modal
-} from "@bcgov/design-system-react-components";
-
-import React, { useState } from 'react';
-import Slider from '@mui/material/Slider';
 
 
 interface HappinessSliderProps {
-    isOpen: boolean;
-    onSelect: (rating: number) => void;
+    value: number;
+    onChange: (value: number) => void;
 }
 
 function HappinessSlider({
-    isOpen,
-    onSelect
+    value,
+    onChange
 }: HappinessSliderProps) {
 
-    const [value, setValue] = useState(3);
+    return (
+        <div>
+            <label>How happy are you?</label>
 
-    if (!isOpen) return null;
-
-    const emojis = [
-        "😡",
-        "🙁",
-        "😐",
-        "🙂",
-        "😁"
-    ];
-
-    const handleSliderChange = (newValue: number) => {
-        setValue(newValue);
-        console.log(newValue);
-    }
-
-    const EmojiHandle = (props: any) => {
-        const handleValue = props.value;
-
-        return (
             <div
-                {...props}
                 style={{
-                    width: "40px",
-                    height: "40px",
-                    fontSize: "2rem",
-                    background: "transparent",
-                    border: "none",
-                    cursor: "grab",
-                    textAlign: "center",
-                    lineHeight: "40px",
-                    marginTop: "-10px"
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "1rem"
                 }}
             >
-                {emojis[handleValue - 1]}
+                <span>😡</span>
+
+                <input
+                    type="range"
+                    min={1}
+                    max={5}
+                    step={1}
+                    value={value}
+                    onChange={(e) =>
+                        onChange(Number(e.target.value))
+                    }
+                />
+
+                <span>😁</span>
             </div>
-        );
-    };
-
-
-    return (
-        <Modal isOpen={isOpen}>
-            <Dialog aria-label="Happiness rating">
-                <div style={{ padding: "1rem" }}>
-                    <h2>How was your experience?</h2>
-
-                    <div
-                        style={{
-                            display: "flex",
-                            gap: "1rem",
-                            justifyContent: "center"
-                        }}
-                    >
-                        {emoji[value - 1]}
-                    </div>
-                    <Slider
-                        min={1}
-                        max={5}
-                        step={1}
-                        value={value}
-
-                        onChange={(newValue) =>
-                            handleSliderChange(newValue as number)
-                        }
-                        onChangeComplete={(newValue) =>
-                            onSelect(newValue as number)
-                        }
-                        handleRender={(node, props) => (
-                            <EmojiHandle {...props} />
-                        )}
-                    />
-
-                    <div>
-                        style=={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            marginTop: "1rem",
-                            fontSize: "2rem"
-                        }}
-
-                        {emojis.map((emoji) => (
-                            <span key={emoji}>{emoji}</span>
-                        ))}
-
-                    </div>
-                </div>
-            </Dialog>
-        </Modal>
+        </div>
     );
 }
-
 
 export default HappinessSlider;
