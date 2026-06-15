eb64d412b111d238faae9eb5ed06c427829ed600
Maia Grisch <thisisanimosity@hotmail.com>
2026-06-15 09:16:59 -0700

    Question config files added (#88)

---
+++ frontend/src/config/questionConfig.ts
@@ -0,0 +1,30 @@
+
+export const questionConfig = {
+    TEXTAREA: {
+        inputType: "textarea",
+    },
+
+    RADIO: {
+        inputType: "radio",
+    },
+
+    DROPDOWN: {
+        inputType: "select",
+    },
+
+    BOOLEAN: {
+        inputType: "boolean",
+    },
+
+    CHECKBOX: {
+        inputType: "checkbox",
+    },
+
+    NPS: {
+        inputType: "nps",
+    },
+
+    SLIDER: {
+        inputType: "slider",
+    },
+};
