64c9451719b9a5c13753c31ffce30bc88fef9848
Ebba <degrootebba@gmail.com>
2026-07-26 14:57:43 -0700

    changed port in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./auth/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <FeedbackProvider apiBaseUrl="http://localhost:3001">
+    <FeedbackProvider apiBaseUrl="http://localhost:3000">
       <App />
     </FeedbackProvider>
   </AuthProvider>
